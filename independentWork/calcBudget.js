ddd6f471d785c0817f5b334a2f8c996d0de602ba
RAman <vv219901@gmail.com>
2020-03-01 16:46:39 +1000

    practice app third parts

--- independentWork/calcBudget.js
+++ independentWork/calcBudget.js
@@ -1,3 +1,4 @@
+(function() {
 "use strict";
 
 let money, time;
@@ -19,11 +20,80 @@ let appDate = {
     optionalExpenses: {},
     incom: [],
     savings: true,
+    shooseExpenses: function() {
+        for (let i = 0; i < 2; i++) {
+            let obligatoryExpenses = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            obligatoryExpensesSum = +prompt("Во сколько это обойдется?", "");
+             if(typeof(obligatoryExpenses) === "string" && typeof(obligatoryExpenses) != null && 
+                     obligatoryExpenses != "" && obligatoryExpenses.length < 50 && obligatoryExpensesSum != "" &&
+                     typeof(obligatoryExpensesSum) != null ) {
+                appDate.expenses[obligatoryExpenses] = obligatoryExpensesSum;
+                console.log("done");
+             } else {
+                i = i -1;
+             }
+        }   
+    },
+    detectDayBudget: function() {
+        appDate.moneyPerDay = (appDate.budget / 30);
+        alert("Ваш ежедневный бюджет равен " + (appDate.moneyPerDay).toFixed(2));
+    },
+    detectLevel: function() {
+        if(appDate.moneyPerDay < 1000) {
+            console.log("Low income");
+        } else if(appDate.moneyPerDay > 1000 && appDate.moneyPerDay < 5000) {
+            console.log("You have an average income");
+        } else if(appDate.moneyPerDay > 5000) {
+            console.log("You have a high level of wealth!");
+        } else {
+            console.log("Error!");
+        }
+    },
+    checkSaving: function() {
+        if(appDate.savings == true) {
+            let save = +prompt("Какая сумма на депозите?"),
+             persent = +prompt("Под какой процент годовых?"); 
+             
+             appDate.monthIncome = save/100/12*persent;
+             alert("Ваш ежемесячный доход с депозита составляет: " + (appDate.monthIncome).toFixed(2));
+         }
+    },
+    chooseOptExpenses: function() {
+        for(let i = 1; i < 3; i++) {
+            let opt = prompt("Введите статью необязательных расходов:", "");
+            if(typeof(opt) === "string" && typeof(opt) != null && opt != "" && opt.length < 50){
+                appDate.optionalExpenses[i] = opt;
+                 } else {
+                     i = i - 1;
+            }
+        }
+    },
+    chooseIncome: function() {
+    for(let i = 1; i < 2; i++) {
+        let inc = prompt('Заполните поле дополнительного дохода (введите статьи дохода через ","', "");
+        if(typeof(inc) === "string" && typeof(inc) != null && inc != "" && inc.length < 50) {
+            appDate.incom = inc.split(", ");
+            appDate.incom.push(prompt("Может что то еще?"));
+            appDate.incom.sort();
+        } else {
+            i = i - 1;
+        }
+      }
+        appDate.incom.forEach((item, index ) => {
+                  alert(`${index + 1}: доп статья дохода ${item}`);
+        });
+    },
 };
 //console.log(appDate);
 //console.log(money);
+appDate.shooseExpenses();
+appDate.detectDayBudget();
+appDate.detectLevel();
+appDate.checkSaving();
+appDate.chooseOptExpenses();
+appDate.chooseIncome();
 
-function shooseExpenses() {
+/*function shooseExpenses() {
     for (let i = 0; i < 2; i++) {
         let obligatoryExpenses = prompt("Введите обязательную статью расходов в этом месяце", ""),
         obligatoryExpensesSum = +prompt("Во сколько это обойдется?", "");
@@ -37,7 +107,7 @@ function shooseExpenses() {
          }
     }
 }
-shooseExpenses();
+shooseExpenses();*/
 
 /*let i = 0;
 while (i < 4) {
@@ -69,13 +139,13 @@ do {
     i++;
 } while (i < 4);*/
 
- function detectDayBudget() {
+ /*function detectDayBudget() {
     appDate.moneyPerDay = (appDate.budget / 30);
-    alert("Ваш ежедневный бюджет равен " + (appDate.moneyPerDay).toFixed());
+    alert("Ваш ежедневный бюджет равен " + (appDate.moneyPerDay).toFixed(2));
   }  
-detectDayBudget();
+detectDayBudget();*/
 
-function detectLevel() {
+/*function detectLevel() {
     if(appDate.moneyPerDay < 1000) {
         console.log("Low income");
     } else if(appDate.moneyPerDay > 1000 && appDate.moneyPerDay < 5000) {
@@ -86,10 +156,10 @@ function detectLevel() {
         console.log("Error!");
     }
 }
-detectLevel();
+detectLevel();*/
 
 
-function checkSaving() {
+/*function checkSaving() {
     if(appDate.savings == true) {
        let save = +prompt("Какая сумма на депозите?"),
         persent = +prompt("Под какой процент годовых?"); 
@@ -98,24 +168,19 @@ function checkSaving() {
         alert("Ваш ежемесячный доход с депозита составляет: " + (appDate.monthIncome).toFixed(2));
     }
 }
-checkSaving();
+checkSaving();*/
 
-function chooseOptExpenses() {
-    let a = prompt("Статья необязательных расходов?"),
-        b = prompt("Статья необязательных расходов?"),
-        c = prompt("Статья необязательных расходов?");
-
-        if(typeof(a) === "string" && typeof(a) != null && a != "" && a.length < 50 &&
-        typeof(b) === "string" && typeof(b) != null && b != "" && b.length < 50 &&
-        typeof(c) === "string" && typeof(c) != null && c != "" && c.length < 50) {
-            appDate.optionalExpenses[1] = a;
-            appDate.optionalExpenses[2] = b;
-            appDate.optionalExpenses[3] = c;
-     } else {
-         alert("Введите корректные даннные!");
-     }
+/*function chooseOptExpenses() {
+    for(let i = 1; i < 3; i++) {
+        let opt = prompt("Введите статью необязательных расходов:", "");
+        if(typeof(opt) === "string" && typeof(opt) != null && opt != "" && opt.length < 50){
+            appDate.optionalExpenses[i] = opt;
+             } else {
+                 i = i - 1;
+        }
+    }
 }
-chooseOptExpenses();
+chooseOptExpenses();*/
 /*function s( expenses ) {
     let rez = 0;
     for( key in expenses ) {
@@ -128,5 +193,9 @@ return rez;
 
 //alert( "Ваш бюджет на один день составит: " + monthlyBudget2);
 
-console.log(appDate);
+for(let key in appDate) {
+        console.log(`DATA: ${appDate[key]}`);
+}
+//console.log(appDate);
 
+}());
